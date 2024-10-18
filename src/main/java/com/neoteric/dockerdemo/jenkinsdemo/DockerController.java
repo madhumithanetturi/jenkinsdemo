c8cfbdd02dbf1d3a15126b476519b1cbfdcea295
madhumithanetturi <madhumithanetturi19@gmail.com>
2024-10-18 18:27:14 +0530

    commit from docker

--- src/main/java/com/neoteric/dockerdemo/jenkinsdemo/DockerController.java
+++ src/main/java/com/neoteric/dockerdemo/jenkinsdemo/DockerController.java
@@ -2,6 +2,6 @@ package src.main.java.com.neoteric.dockerdemo.jenkinsdemo;
 
 public class DockerController {
     public String hello(){
-        return "hello world";
+        return "hello to my world";
     }
 }
